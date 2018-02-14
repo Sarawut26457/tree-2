e87199bb607399823bb6a9bf26693132fbcf2803
unknown <Sarawut26457>
2018-02-14 22:17:36 +0700

    sss

---
+++ src/main/java/rmuti/LO/Visitor.java
@@ -0,0 +1,6 @@
+package rmuti.LO;
+
+public interface Visitor {
+
+	public void visit(Object e);
+}
---
+++ src/main/java/rmuti/ds/Tree.java
@@ -0,0 +1,35 @@
+package rmuti.ds;
+
+
+import rmuti.LO.Visitor;
+import rmuti.ds.Node;
+
+
+public class Tree {
+	public void preOrder(Node r,Visitor v) {
+		if(r==null) {
+			return;
+		}
+		v.visit(r.getValue());
+		preOrder(r.left,v);
+		preOrder(r.right,v);
+	}
+	public void inOrder(Node r,Visitor v) {
+		if(r==null) {
+			return;
+	}
+		inOrder(r.left,v);
+		v.visit(r.getValue());
+		preOrder(r.right,v);
+	}
+	public void postOrder(Node r,Visitor v){
+		if(r==null) {
+			return;
+	}
+		preOrder(r.left,v);
+		preOrder(r.right,v);
+		v.visit(r.getValue());
+	}
+
+
+}
